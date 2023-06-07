48c739a4fe29ab9e6dba9df63e6d6e16feeadcd7
codeXplorerePo <codexplore.vuchinam97@gmail.com>
2023-06-07 15:58:50 +0700

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  firebase: {
+    projectId: 'angular-login-with-fireb-8a009',
+    appId: '1:388257179238:web:80b57875129b51d8bcafa7',
+    storageBucket: 'angular-login-with-fireb-8a009.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyDK0asxLEKlnyNHwvWzn-vfRDVy6hFaubk',
+    authDomain: 'angular-login-with-fireb-8a009.firebaseapp.com',
+    messagingSenderId: '388257179238',
+    measurementId: 'G-4QKP3FVXHD',
+  },
+  production: true,
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebase: {
+    projectId: 'angular-login-with-fireb-8a009',
+    appId: '1:388257179238:web:80b57875129b51d8bcafa7',
+    storageBucket: 'angular-login-with-fireb-8a009.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyDK0asxLEKlnyNHwvWzn-vfRDVy6hFaubk',
+    authDomain: 'angular-login-with-fireb-8a009.firebaseapp.com',
+    messagingSenderId: '388257179238',
+    measurementId: 'G-4QKP3FVXHD',
+  },
+  production: false,
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
